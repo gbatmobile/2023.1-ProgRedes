28d0ee72f29ba91846e36050733cb26d355b7355
GALILEU Batista <gbatmobile@gmail.com>
2023-05-22 22:37:54 -0300

    Aula 2023/05/22

---
+++ TCP-Echo/echo-client-tcp.py
@@ -0,0 +1,19 @@
+import socket
+
+SERVER = '127.0.0.1'
+PORT = 50000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((SERVER, PORT))
+
+while True:
+    msg = input("Digite mensagem: ")
+    if msg != '':
+        msgB = msg.encode('utf-8')
+        sock.send (msgB)
+
+        data = sock.recv(4096)
+        msg = data.decode('utf-8')
+        print (f"Echo recebido: {msg} ")
+
+sock.close()
---
+++ TCP-Echo/echo-server-tcp.py
@@ -0,0 +1,23 @@
+import socket
+
+SERVER = '127.0.0.1'
+PORT = 50000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind((SERVER, PORT))
+sock.listen(1)
+print ("aceitando conexoes ...")
+
+con, client = sock.accept()
+print ("conexão estabelecida ...")
+
+while True:
+    data = con.recv (4096)
+    msg = f"received {data} from {client} ..."
+
+    print (msg, " devolvendo ...")
+    con.send (data) # msg.encode('utf-8'))
+
+con.close()
+sock.close()
